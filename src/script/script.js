2671281c416a520668dc7df0e5b65721106feb69
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-03-11 01:58:35 +0300

    feat : mouse gestures is created

--- src/script/script.js
+++ src/script/script.js
@@ -16,9 +16,12 @@ const alertControl = () => {
 
 
 const container = document.querySelector('body');
-let r = document.body.style.backgroundColor;
-let g = document.body.style.backgroundColor;
-let b = document.body.style.backgroundColor;
+const bgval = document.body.style.backgroundColor;
+let r = bgval;
+let g = bgval;
+let b = bgval;
+let a = 1;
+let color = '#f0f0f0f0'
 let isLeftToRight = true;
 
 container.addEventListener('mousemove', (e) => {
@@ -32,24 +35,49 @@ container.addEventListener('mousemove', (e) => {
   }
   if (isLeftToRight && Math.abs(e.movementX) > Math.abs(e.movementY)) 
   {
-    r -= 4;
-    g -= 3;
-    b -= 2;
-    
+    r -= 3;
+    g -= 2;
+    b -= 1;
   } 
   else if (!isLeftToRight && Math.abs(e.movementX) > Math.abs(e.movementY)) 
   {
-    r += 4;
-    g += 3;
-    b += 2;
+    r += 3;
+    g += 2;
+    b += 1;
   }
+  if (e.movementY > 0)
+  {
+    a +=0.01;
+  }
+  else
+  {
+    a -=0.01;
+  }
+  if (e.movementY > 0) {
+    color = lightenColor(color, 0.1);
+  } else {
+    color = darkenColor(color, 0.1);
+  }
+  container.style.backgroundColor = color;
+
+  const hexValue = document.querySelector(".hexactive");
+    hexValue.placeholder = color;
+
   r = Math.min(Math.max(r, 0), 255);
   g = Math.min(Math.max(g, 0), 255);
   b = Math.min(Math.max(b, 0), 255);
-  container.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  a = Math.min(Math.max(a, 0), 1);
+  container.style.backgroundColor = `rgb(${r}, ${g}, ${b}, ${a})`;
+
+    const rgbValue = document.querySelector(".rgbactive");
+    rgbValue.placeholder = `rgb(${r}, ${g}, ${b})`;
+
+    const rgbaValue = document.querySelector(".rgbaactive");
+    rgbaValue.placeholder = `rgba(${r}, ${g}, ${b}, ${a})`;
+
+    
+
 });
-
-
 
 document.addEventListener('mousedown', (e) => {
     e.preventDefault();
@@ -65,15 +93,40 @@ document.addEventListener('mousedown', (e) => {
             window.addEventListener('click', alertControl);
 
         }, () => {
-            alert("Seçmiş olduğunuz renk panoya kopyalanamdı. Lütfen tekrar deneyiniz.");
+            alert("Seçmiş olduğunuz renk panoya kopyalanamadı. Lütfen tekrar deneyiniz.");
         })
     }
 })
 
-document.addEventListener('mousemove', (e) => {
-    let bgc = document.body.style.backgroundColor;
-    //console.log(bgc);
-    const rgbValue = document.querySelector(".rgbactive");
-    rgbValue.placeholder = bgc;
-    document.body.appendChild(colorInfo);
-})
+
+container.addEventListener('mousemove', function (e) {
+  
+});
+
+const lightenColor = (hex, amount) => {
+  let [r, g, b] = hexToRgb(hex);
+  r += Math.round((255 - r) * amount);
+  g += Math.round((255 - g) * amount);
+  b += Math.round((255 - b) * amount);
+  return rgbToHex(r, g, b);
+}
+
+const darkenColor = (hex, amount) => {
+  let [r, g, b] = hexToRgb(hex);
+  r -= Math.round(r * amount);
+  g -= Math.round(g * amount);
+  b -= Math.round(b * amount);
+  return rgbToHex(r, g, b);
+}
+
+const hexToRgb = (hex) => {
+  const bigint = parseInt(hex.slice(1), 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return [r, g, b];
+}
+
+const rgbToHex =(r, g, b) => {
+  return `#${((r << 16) | (g << 8) | b).toString(16).padStart(6, '0')}`;
+}
